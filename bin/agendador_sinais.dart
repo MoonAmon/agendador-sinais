19147ea6c97a8bade6ceed76e3075eb077b6e0a3
MoonAmon <thiagoamancio.365@gmail.com>
2025-06-27 12:37:15 -0300

    innit commit

---
+++ bin/agendador_sinais.dart
@@ -0,0 +1,5 @@
+import 'package:agendador_sinais/agendador_sinais.dart' as agendador_sinais;
+
+void main(List<String> arguments) {
+  print('Hello world: ${agendador_sinais.calculate()}!');
+}
---
+++ test/agendador_sinais_test.dart
@@ -0,0 +1,8 @@
+import 'package:agendador_sinais/agendador_sinais.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
